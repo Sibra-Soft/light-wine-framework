b0e3e395459fd0110e5120c3ac3e11e20bbef000
Alex van den Berg <info@sibra-soft.nl>
2022-03-12 20:42:12 +0100

    Added new function and parameters and removed all functions

--- src/Core/Interfaces/IComponentBase.php
+++ src/Core/Interfaces/IComponentBase.php
@@ -3,6 +3,10 @@ namespace LightWine\Core\Interfaces;
 
 interface IComponentBase
 {
-    public function GetSettings(string $setting): string;
-    public function GetControlTemplate(string $name): string;
+    /**
+     * Gets the settings of the specified component, and adds them to the model
+     * @param mixed $controlInstance The instance of the component, you want to get the settings from
+     * @param int $componentId The id of the component in the database
+     */
+    public function GetSettings($controlInstance, int $componentId): object;
 }
