5afd3fa297e90e5ed48c3b3c05ff7d98de478986
Alex van den Berg <info@sibra-soft.nl>
2022-06-23 12:06:58 +0200

    Improvements for the StringHelpers class

--- src/Core/Helpers/StringHelpers.php
+++ src/Core/Helpers/StringHelpers.php
@@ -1,6 +1,8 @@
 <?php
 namespace LightWine\Core\Helpers;
 
+use \DateTime;
+
 class StringHelpers
 {
     /**
@@ -19,7 +21,12 @@ class StringHelpers
         }
     }
 
-    public static function IsValidDate($date): bool {
+    /**
+     * Checks if the specified date is valid
+     * @param string $date The date you want to check
+     * @return bool Returns true if the date is valid, false if the date is not valid
+     */
+    public static function IsValidDate(string $date): bool {
         $format = "Y-m-d";
         $d = DateTime::createFromFormat($format, $date);
         return $d && $d->format($format) === $date;
@@ -27,9 +34,9 @@ class StringHelpers
 
     /**
      * Add leading zeros to a specified integer
-     * @param integer $integer
-     * @param integer $numberOfZeros
-     * @return string
+     * @param integer $integer The integer you want to add zero's to
+     * @param integer $numberOfZeros The number of zero's you want to add
+     * @return string String containg the integer with added zero's
      */
     public static function Pad(int $integer, int $numberOfZeros = 2):string {
         return sprintf('%0'.$numberOfZeros.'d', $integer);
@@ -37,27 +44,27 @@ class StringHelpers
 
     /**
      * This function can split a string and return the value at a posistion
-     * @param string $value
-     * @param string $delimiter
-     * @param integer $posistion
-     * @return string
+     * @param string $value The string you want to split using a delimiter
+     * @param string $delimiter The delimiter that must be used
+     * @param integer $position The position of tht string you want to return
+     * @return string The string that can be found at the specified position using the specified delimiter
      */
-    public static function SplitString(string $value, string $delimiter, int $posistion): string{
+    public static function SplitString(string $value, string $delimiter, int $position): string{
         $splitString = explode($delimiter, $value);
 
-        if(StringHelpers::IsNullOrWhiteSpace($splitString[$posistion])){
+        if(StringHelpers::IsNullOrWhiteSpace($splitString[$position])){
             return "";
         }else{
-            return $splitString[$posistion];
+            return $splitString[$position];
         }
     }
 
     /**
      * Gets the string between two other strings, for example <tag>test</tag> the output would be test
-     * @param string $string
-     * @param string $start
-     * @param string $end
-     * @return string
+     * @param string $string The string you want to use
+     * @param string $start The beginning char
+     * @param string $end The ending char
+     * @return string The string that can be found between the beginning char and ending char
      */
     public static function StringBetween(string $string, string $start, string $end){
         $string = ' ' . $string;
@@ -71,10 +78,10 @@ class StringHelpers
 
     /**
      * This function gets a string between the start integer and end integer
-     * @param string $string
-     * @param integer $start
-     * @param integer $end
-     * @return string
+     * @param string $string The string you want to use
+     * @param integer $start The start position
+     * @param integer $end The ending position
+     * @return string The start that can be found between the specified start position and ending position
      */
     public static function Mid($string, $start, $end){
         $length = $start - $end;
@@ -83,29 +90,10 @@ class StringHelpers
     }
 
     /**
-     * This function gets string between two string
-     * @param string $string
-     * @param integer $start
-     * @param integer $end
-     * @return string
-     */
-    public static function Between($string, $start, $end){
-        $string = ' ' . $string;
-        $ini = strpos($string, $start);
-
-        if ($ini == 0) return '';
-
-        $ini += strlen($start);
-        $len = strpos($string, $end, $ini) - $ini;
-
-        return substr($string, $ini, $len);
-    }
-
-    /**
      * Join array parts with a specified delimiter
-     * @param mixed $subject
-     * @param string $delimiter
-     * @return string
+     * @param mixed $subject A array containg the values you want to join
+     * @param string $delimiter The delimiter that must be used when joining
+     * @return string The string containing the joined array
      */
     public static function JoinString($subject, string $delimiter):string {
         if(StringHelpers::IsNullOrWhiteSpace($subject)){
@@ -127,7 +115,7 @@ class StringHelpers
      * Function to check if the given string starts with the specified value
      * @param string $haystack The subject string
      * @param string $needle The string to search in the subject
-     * @return boolean
+     * @return boolean True if the specifid string begins with the specified begingging
      */
     public static function StartsWith(string $haystack, string $needle):bool {
         $length = strlen($needle);
@@ -138,7 +126,7 @@ class StringHelpers
      * Function to check if the given string ends with the specified value
      * @param string $haystack The subject string
      * @param string $needle The string to search in the subject
-     * @return boolean
+     * @return boolean True if the specified string end with the specifeid ending
      */
     public static function EndsWith(string $haystack, string $needle):bool {
         $length = strlen($needle);
@@ -151,11 +139,11 @@ class StringHelpers
 
     /**
      * Function to check if a given string contains a specified value
-     * @param string $haystack
-     * @param string $needle
-     * @return boolean
+     * @param string $haystack The string you want to check
+     * @param string $needle The string you want to check if exists in the specified string
+     * @return boolean True if the needle can be found in the haystack
      */
-    public static function Contains($haystack, $needle):bool {
+    public static function Contains(string $haystack, string $needle): bool {
         if (strpos($haystack, $needle) !== false) {
             return true;
         }else{
@@ -166,7 +154,7 @@ class StringHelpers
     /**
      * Function for checking if the specified value is a empty string
      * @param mixed $value The value you want to check
-     * @return boolean
+     * @return boolean True if the valud is null or whitespace
      */
     public static function IsNullOrWhiteSpace($value):bool {
         if($value == "" or $value == null){
@@ -177,22 +165,10 @@ class StringHelpers
     }
 
     /**
-     * Escape a string from special chars
-     * @param string $value
-     * @return string
-     */
-    public static function Escape($value){
-        $search = array("\\",  "\x00", "\n",  "\r",  "'",  '"', "\x1a");
-        $replace = array("\\\\","\\0","\\n", "\\r", "\'", '\"', "\\Z");
-
-        return str_replace($search, $replace, $value);
-    }
-
-    /**
      * Truncate a specfied string to a number of charters and add dots if longer
-     * @param string $inputString
-     * @param integer $maxLength
-     * @return string
+     * @param string $inputString The string you want to ellipis
+     * @param integer $maxLength The position you want to start adding the dots
+     * @return string Changed string containg dots at the specified position
      */
     public static function TruncateEllipsis(string $inputString, int $maxLength):string {
         if(strlen($inputString) > $maxLength){
