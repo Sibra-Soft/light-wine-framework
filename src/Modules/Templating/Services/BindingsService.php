b7b4f5205485b928477a805f3d9ee80d36eab6ee
Alex van den Berg <alex@sibra-soft.nl>
2023-12-08 23:18:43 +0100

    Changed bindings to templates
    Bindings are now added as templates to the CMS

--- src/Modules/Templating/Services/BindingsService.php
+++ src/Modules/Templating/Services/BindingsService.php
@@ -24,12 +24,15 @@ class BindingsService implements IBindingService
         $this->databaseConnection->AddParameter("templateId", $templateId);
         $dataset = $this->databaseConnection->GetDataset("
             SELECT
-                binding.result_can_be_empty AS `can_be_empty`,
-	            binding.`name` AS `binding_name`,
-	            binding.source_template_id AS `binding_template_id`,
-                binding.result_as_json AS `result_as_json`
-            FROM `site_bindings` AS binding
-            WHERE binding.destination_template_id = ?templateId
+	            bindings.`name`,
+	            JSON_EXTRACT(version.content, '$.template') AS binding_template_id,
+	            JSON_UNQUOTE(JSON_EXTRACT(version.content, '$.type')) AS type,
+	            JSON_EXTRACT(version.content, '$.result_can_be_empty') AS result_can_be_empty,
+	            JSON_EXTRACT(version.content, '$.result_as_json') AS result_as_json
+            FROM site_templates AS bindings
+            INNER JOIN site_template_versioning AS version ON version.template_id = bindings.id AND version.version = bindings.template_version_dev
+            WHERE bindings.parent_id = ?templateId
+	            AND bindings.type = 'binding'
 	    ");
 
         $returnModel->BindingTemplateId = $templateId;
@@ -38,12 +41,12 @@ class BindingsService implements IBindingService
         // Only add databinding data if bindings are found
         if($this->databaseConnection->rowCount > 0){
             foreach($dataset as $row){
-                $bindingName = $row["binding_name"];
+                $bindingName = $row["name"];
                 $bindingQuery = $row["binding_template_id"];
 
                 $queryTemplate = $this->templatingService->RenderTemplateAndDoAllReplacements($bindingQuery)->Content;
 
-                if(StringHelpers::IsNullOrWhiteSpace($queryTemplate) && $row["can_be_empty"] == 1){
+                if(StringHelpers::IsNullOrWhiteSpace($queryTemplate) && $row["result_can_be_empty"] == 1){
                     $returnModel->BindingResult[$bindingName."_rowcount"] = 0;
                     $returnModel->BindingResult[$bindingName] = json_decode(json_encode([[]]));
                 }else{
