36f9a3c26e31942e93137c84fbc8b8e62deac6a3
Alex van den Berg <alex@sibra-soft.nl>
2023-12-12 19:24:11 +0100

    Added String and Object as return type

--- src/Modules/ConfigurationManager/Services/ConfigurationManagerService.php
+++ src/Modules/ConfigurationManager/Services/ConfigurationManagerService.php
@@ -35,9 +35,9 @@ class ConfigurationManagerService implements IConfigurationManagerService
     /**
      * Gets a specified application setting from the config
      * @param string $name The name of the setting
-     * @return string|array The value of the requested setting
+     * @return string|object The value of the requested setting
      */
-    public function GetAppSetting(string $name, string $default = ""){
+    public function GetAppSetting(string $name, string $default = ""): string|object {
         if(array_key_exists($name, $this->Settings)){
             return $this->Settings[$name];
         }else{
