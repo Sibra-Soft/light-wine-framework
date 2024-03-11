02422abb9c6a3922e62ed0c612e15f381f2731d5
Alex van den Berg <alex@sibra-soft.nl>
2024-03-11 10:55:19 +0100

    Added new token security

--- src/Providers/SchedulerServiceProvider/SchedulerServiceProvider.php
+++ src/Providers/SchedulerServiceProvider/SchedulerServiceProvider.php
@@ -1,19 +1,30 @@
 <?php
 namespace LightWine\Providers\SchedulerServiceProvider;
 
+use LightWine\Core\Helpers\RequestVariables;
 use LightWine\Core\HttpResponse;
+use LightWine\Modules\ConfigurationManager\Services\ConfigurationManagerService;
 use LightWine\Modules\Scheduler\Services\SchedulerService;
 
 class SchedulerServiceProvider
 {
     private SchedulerService $scheduler;
+    private ConfigurationManagerService $settings;
 
     public function __construct(){
+        $this->settings = new ConfigurationManagerService();
         $this->scheduler = new SchedulerService();
     }
 
     public function Render(){
-        HttpResponse::SetData($this->scheduler->CheckForScheduledEvents());
-        exit();
+        $token = $this->settings->GetAppSetting("scheduler")["token"];
+        $requestToken = RequestVariables::Get("token");
+
+        if($token !== $requestToken){
+            Throw new \Exception("Invalid token");
+        }else{
+            HttpResponse::SetData($this->scheduler->CheckForScheduledEvents());
+            exit();
+        }
     }
 }
