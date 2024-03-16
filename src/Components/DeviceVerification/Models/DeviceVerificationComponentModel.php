57ab29f4e2ce64650bdbd5cd0798146d9d16581c
Alex van den Berg <alex@sibra-soft.nl>
2024-03-16 13:42:20 +0100

    Refactor of the DeviceVerification component

--- src/Components/DeviceVerification/Models/DeviceVerificationComponentModel.php
+++ src/Components/DeviceVerification/Models/DeviceVerificationComponentModel.php
@@ -4,18 +4,22 @@ use LightWine\Components\DeviceVerification\Enums\ComponentModes;
 
 class DeviceVerificationComponentModel
 {
-    public int $Mode = ComponentModes::Check;
+    public int $Mode = ComponentModes::Verify;
+    public int $MailTemplate = 0;
 
     public string $CheckTemplate = "";
     public string $VerifyTemplate = "";
-    public string $MailTemplate = "";
     public string $SmsTemplate = "";
     public string $ConfirmTemplate = "";
     public string $RedirectUrlIfVerified = "";
+    public string $RedirectUrlIfNotVerified = "";
     public string $ErrorTemplate = "";
 
     public bool $UseLinkToVerify = true;
     public bool $UsePincodeToVerify = false;
+
     public bool $SendMailToVerify = true;
     public bool $SendSmsToVerify = false;
+
+    public bool $RedirectIfVerified = false;
 }
