f06449a4358fd628945fd3cb2800df0b9e28e422
Alex van den Berg <alex@sibra-soft.nl>
2024-03-19 20:36:10 +0100

    Added functionality for hiding errors and exceptions

--- src/Bootloader.php
+++ src/Bootloader.php
@@ -78,19 +78,23 @@ class Bootloader {
      * @param TypeError|Exception|Error $exception The thrown exception
      */
     public function SetExceptionHandler($exception){
-        $view = Helpers::GetFileContent("~/src/Views/Exception.tpl");
-		$composerJson = json_decode(Helpers::GetFileContent("#/composer.json"), false);
-		
-        $message = StringHelpers::SplitString($exception->getMessage(), "#", 0);
-        $specifiedSource = StringHelpers::SplitString($exception->getMessage(), "#", 1);
+        if(ini_get("display_errors")){
+            $view = Helpers::GetFileContent("~/src/Views/Exception.tpl");
+            $composerJson = json_decode(Helpers::GetFileContent("#/composer.json"), false);
 
-        if(StringHelpers::IsNullOrWhiteSpace($specifiedSource)) $specifiedSource = $exception->getTraceAsString();
+            $message = StringHelpers::SplitString($exception->getMessage(), "#", 0);
+            $specifiedSource = StringHelpers::SplitString($exception->getMessage(), "#", 1);
 
-        $view = str_replace("{{source_file_line}}", $exception->getLine(), $view);
-        $view = str_replace("{{source_file}}", $exception->getFile(), $view);
-        $view = str_replace("{{error_message}}", $message, $view);
-        $view = str_replace("{{source}}", $specifiedSource, $view);
-		$view = str_replace("{{framework_version}}", $composerJson->version, $view);
+            if(StringHelpers::IsNullOrWhiteSpace($specifiedSource)) $specifiedSource = $exception->getTraceAsString();
+
+            $view = str_replace("{{source_file_line}}", $exception->getLine(), $view);
+            $view = str_replace("{{source_file}}", $exception->getFile(), $view);
+            $view = str_replace("{{error_message}}", $message, $view);
+            $view = str_replace("{{source}}", $specifiedSource, $view);
+            $view = str_replace("{{framework_version}}", $composerJson->version, $view);
+        }else{
+            $view = "An error has occurred";
+        }
 
         http_response_code(500);
         HttpResponse::SetContentType("text/html");
